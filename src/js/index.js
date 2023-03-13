0fef60c1073e7171b90194d42d7baa4ff0ae0245
Tuts9 <127217432+Tuts9@users.noreply.github.com>
2023-03-13 13:08:47 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+let indiceAtual = 0;
+
+function mudarImagem() {
+  const botaoAtual = document.querySelector('.selecionado');
+  botaoAtual.classList.remove('selecionado');
+  imagens[indiceAtual].classList.remove('ativa');
+  indiceAtual = (indiceAtual + 1) % imagens.length;
+  imagens[indiceAtual].classList.add('ativa');
+  botoesCarrossel[indiceAtual].classList.add('selecionado');
+}
+
+let temporizador = setInterval(mudarImagem, 5000);
+
+botoesCarrossel.forEach((botao, indice) => {
+  botao.addEventListener('click', () => {
+    clearInterval(temporizador);
+    const botaoAtual = document.querySelector('.selecionado');
+    botaoAtual.classList.remove('selecionado');
+    botao.classList.add('selecionado');
+    imagens[indiceAtual].classList.remove('ativa');
+    indiceAtual = indice;
+    imagens[indiceAtual].classList.add('ativa');
+    temporizador = setInterval(mudarImagem, 5000);
+  });
+});
